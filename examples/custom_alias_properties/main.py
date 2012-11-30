df888959f4f58026ef1ba2d47c051f68234a1f22
dhermes <dhermes@google.com>
2012-11-29 16:17:31 -0800

    Renaming default query order sample and adding some other functionality.

--- examples/custom_alias_properties/main.py
+++ examples/custom_alias_properties/main.py
@@ -1,4 +1,7 @@
+import random
+
 from google.appengine.ext import ndb
+from protorpc import messages
 from protorpc import remote
 
 import endpoints
@@ -10,10 +13,33 @@ from endpoints_proto_datastore.ndb import EndpointsModel
 DEFAULT_ORDER = 'attr1,-attr2'
 
 
+def RandomId():
+  hex_digits = '0123456789ABCDEFG'
+  return ''.join(random.choice(hex_digits) for _ in range(8))
+
+
 class MyModel(EndpointsModel):
+  _ordering = ('id', 'attr1', 'attr2', 'created')
+
   attr1 = ndb.StringProperty()
   attr2 = ndb.StringProperty()
   created = ndb.DateTimeProperty(auto_now_add=True)
+
+  def _IdSet(self, value):
+    if not isinstance(value, basestring):
+      raise TypeError('ID must be an string.')
+
+    self._key = ndb.Key(self.__class__, value)
+    entity = self._key.get()
+    if entity is not None:
+      self._CopyFromEntity(entity)
+      self._from_datastore = True
+
+  @EndpointsAliasProperty(property_type=messages.StringField,
+                          setter=_IdSet, exempt=True)
+  def id(self):
+    if self._key is not None:
+      return self._key.string_id()
 
   @EndpointsAliasProperty(setter=EndpointsModel._OrderSet,
                           default=DEFAULT_ORDER, exempt=True)
@@ -26,6 +52,8 @@ class MyApi(remote.Service):
 
   @MyModel.method(path='mymodel', http_method='POST', name='mymodel.insert')
   def MyModelInsert(self, my_model):
+    if my_model.key is None:
+      my_model.key = ndb.Key(MyModel, RandomId())
     my_model.put()
     return my_model
 
