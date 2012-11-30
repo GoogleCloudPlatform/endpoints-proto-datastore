105cacb60b80ef8c0db4ae0f5779ddb97927d380
dhermes <dhermes@google.com>
2012-11-30 11:17:53 -0800

    Adding sample which shows how to ensure order queries have an index backed them.

---
+++ examples/protect_indexes/main.py
@@ -0,0 +1,58 @@
+from google.appengine.ext import ndb
+from protorpc import messages
+from protorpc import remote
+
+import endpoints
+
+from endpoints_proto_datastore.ndb import EndpointsAliasProperty
+from endpoints_proto_datastore.ndb import EndpointsModel
+
+
+class Order(messages.Enum):
+  MYFIRST = 1
+  MYSECOND = 2
+
+
+class MyModel(EndpointsModel):
+  attr1 = ndb.StringProperty(required=True)
+  attr2 = ndb.StringProperty(required=True)
+  owner = ndb.UserProperty(required=True)
+  created = ndb.DateTimeProperty(auto_now_add=True)
+
+  def _OrderSet(self, value):
+    if not isinstance(value, Order):
+      raise TypeError('Expected an enum, received: %s.' % (value,))
+
+    if value == Order.MYFIRST:
+      super(MyModel, self)._OrderSet('attr1')
+    elif value == Order.MYSECOND:
+      super(MyModel, self)._OrderSet('-attr2')
+    else:
+      raise TypeError('Unexpected value of Order: %s.' % (value,))
+
+  @EndpointsAliasProperty(property_type=Order, setter=_OrderSet,
+                          default=Order.MYFIRST, exempt=True)
+  def order(self):
+    return super(MyModel, self).order
+
+
+@endpoints.api(name='myapi', version='v1', description='My Little API',
+               audiences=[endpoints.API_EXPLORER_CLIENT_ID])
+class MyApi(remote.Service):
+
+  @MyModel.method(request_ordering=('attr1', 'attr2'),
+                  user_required=True,
+                  path='mymodel', http_method='POST', name='mymodel.insert')
+  def MyModelInsert(self, my_model):
+    my_model.owner = endpoints.get_current_user()
+    my_model.put()
+    return my_model
+
+  @MyModel.query_method(query_ordering=('limit', 'order', 'pageToken'),
+                        user_required=True,
+                        path='mymodels', name='mymodel.list')
+  def MyModelList(self, query):
+    return query.filter(MyModel.owner == endpoints.get_current_user())
+
+
+application = endpoints.api_server([MyApi], restricted=False)
