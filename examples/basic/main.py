b0f7dc89a867382dfa03f60972236450381c4029
dhermes <dhermes@google.com>
2012-12-06 15:34:36 -0800

    Adding comments to document all app.yaml and also to document main in basic and basic with auth.
    Reviewed in https://codereview.appspot.com/6894049/

--- examples/basic/main.py
+++ examples/basic/main.py
@@ -6,23 +6,60 @@ import endpoints
 from endpoints_proto_datastore.ndb import EndpointsModel
 
 
+# Transitioning an existing model is as easy as replacing ndb.Model with
+# EndpointsModel. Since EndpointsModel inherits from ndb.Model, you will have
+# the same behavior and more functionality.
 class MyModel(EndpointsModel):
+  # By default, the ProtoRPC message schema corresponding to this model will
+  # have three string fields: attr1, attr2 and created
+  # in an arbitrary order (the ordering of properties in a dict is not
+  # guaranteed).
   attr1 = ndb.StringProperty()
   attr2 = ndb.StringProperty()
   created = ndb.DateTimeProperty(auto_now_add=True)
 
 
+# Use of this decorator is the same for APIs created with or without
+# endpoints-proto-datastore.
 @endpoints.api(name='myapi', version='v1', description='My Little API')
 class MyApi(remote.Service):
 
+  # Instead of the endpoints.method decorator, we can use MyModel.method to
+  # to define a new endpoints method, but instead of having to convert a
+  # ProtoRPC request message into an entity of our model and back again, we
+  # start out with a MyModel entity and simply have to return one.
+  # Since no overrides for the schema are specified in this decorator, the
+  # request and response ProtoRPC message definition will have the three string
+  # fields attr1, attr2 and created.
   @MyModel.method(path='mymodel', http_method='POST', name='mymodel.insert')
   def MyModelInsert(self, my_model):
+    # Though we don't actively change the model passed in, two things happen:
+    # - The entity gets an ID and is persisted
+    # - Since created is auto_now_add, the entity gets a new value for created
     my_model.put()
     return my_model
 
+  # As MyModel.method replaces a ProtoRPC request message to an entity of our
+  # model, MyModel.query_method replaces it with a query object for our Model.
+  # By default, this query will take no arguments (the ProtoRPC request message
+  # is empty) and will return a response with two fields: items and
+  # nextPageToken. "nextPageToken" is simply a string field for paging through
+  # result sets. "items" is what is called a "MessageField", meaning its value
+  # is a ProtoRPC message itself; it is also a repeated field, meaning we have
+  # an array of values rather than a single value. The nested ProtoRPC message
+  # in the definition of "items" uses the same schema in MyModel.method, so each
+  # value in the "items" array will have the fields attr1, attr2 and created.
+  # As with MyModel.method, overrides can be specified for both the schema of
+  # the request that defines the query and the schema of the messages contained
+  # in the "items" list. We'll see how to use these in further examples.
   @MyModel.query_method(path='mymodels', name='mymodel.list')
   def MyModelList(self, query):
+    # We have no filters that we need to apply, so we just return the query
+    # object as is. As we'll see in further examples, we can augment the query
+    # using environment variables and other parts of the request state.
     return query
 
 
+# Use of endpoints.api_server is the same for APIs created with or without
+# endpoints-proto-datastore.
 application = endpoints.api_server([MyApi], restricted=False)
--- examples/basic_with_auth/main.py
+++ examples/basic_with_auth/main.py
@@ -1,3 +1,5 @@
+# If you have not yet seen the source in basic/main.py, please take a look.
+
 from google.appengine.ext import ndb
 from protorpc import remote
 
@@ -6,27 +8,68 @@ import endpoints
 from endpoints_proto_datastore.ndb import EndpointsModel
 
 
+# In this model definition, we have added an extra field "owner" to the model
+# defined in basic/main.py. Since using auth, we will save the current user and
+# query by the current user, so saving a user property on each entity will allow
+# us to do this.
 class MyModel(EndpointsModel):
+  # By default, the ProtoRPC message schema corresponding to this model will
+  # have four fields: attr1, attr2, created and owner
+  # in an arbitrary order (the ordering of properties in a dict is not
+  # guaranteed).
   attr1 = ndb.StringProperty()
   attr2 = ndb.StringProperty()
-  owner = ndb.UserProperty()
   created = ndb.DateTimeProperty(auto_now_add=True)
+  # The three properties above are represented by string fields, but the
+  # UserProperty below is represented in the ProtoRPC message schema as a
+  # message field -- a field whose value is itself a message. To hold a user
+  # property, a custom ProtoRPC message class is defined in
+  # endpoints_proto_datastore.utils and is used to convert to and from the NDB
+  # property and the corresponding ProtoRPC field.
+  owner = ndb.UserProperty()
 
 
+# Since we are using auth and want to test with the Google APIs Explorer:
+# https://developers.google.com/apis-explorer/
+# we include it's client ID in audiences. This is necessary for auth tokens
+# obtained by the API Explorer (on behalf of users) to be considered valid by
+# our API.
 @endpoints.api(name='myapi', version='v1', description='My Little API',
                audiences=[endpoints.API_EXPLORER_CLIENT_ID])
 class MyApi(remote.Service):
 
+  # To specify that this method requires authentication, we can simply set the
+  # keyword argument user_required to True in the MyModel.method decorator. The
+  # remaining arguments to the decorator are the same as in basic/main.py. Once
+  # user_required is set, the method will first determine if a user has been
+  # detected from the token sent with the request (if any was sent it all) and
+  # will return an HTTP 401 Unauthorized if no valid user is detected. In the
+  # case of a 401, the method will not be executed. Conversely, if method
+  # execution occurs, user_required=True will guarantee that the current user is
+  # valid.
   @MyModel.method(user_required=True,
                   path='mymodel', http_method='POST', name='mymodel.insert')
   def MyModelInsert(self, my_model):
+    # Since user_required is True, we know endpoints.get_current_user will
+    # return a valid user.
     my_model.owner = endpoints.get_current_user()
+    # Also note, since we don't override the default ProtoRPC message schema,
+    # API users can send an owner object in the request, but we overwrite the
+    # model property with the current user before the entity is inserted into
+    # the datastore and this put operation will only occur if a valid token
+    # identifying the user was sent in the Authorization header.
     my_model.put()
     return my_model
 
+  # As above with MyModelInsert, we add user_required=True to the arguments
+  # passed to the MyModel.query_method decorator in basic/main.py. Therefore,
+  # only queries can be made by a valid user.
   @MyModel.query_method(user_required=True,
                         path='mymodels', name='mymodel.list')
   def MyModelList(self, query):
+    # We only allow users to query the MyModel entities that they have created,
+    # so query using owner equal to the current user. Since user_required is
+    # set, we know get_current_user will return a valid user.
     return query.filter(MyModel.owner == endpoints.get_current_user())
 
 
